e272de9a17bcdbb8160bb350e4a7084e4d4543a4
Arthur Couto <acm2@aluno.ifal.edu.br>
2022-05-05 14:39:40 -0300

    aula tal sei láaaa

--- public/js/calc_functions.js
+++ public/js/calc_functions.js
@@ -1,7 +1,20 @@
+function brktSwitch(btn){
+    if (btn.getAttribute('direction') === 'left'){
+        atualizarDisplay(btn);
+        btn.setAttrbute('direction', 'right');
+        btn.setAttrbute('jsValue', ')');
+    } else if (btn.getAttribute('direction') === 'right'){
+        atualizarDisplay(btn);
+        btn.setAttrbute('direction', 'left');
+        btn.setAttrbute('jsValue', '(');
+    }
+}
+
 function atualizarDisplay(btn){
     const display = document.getElementById('display');
     if(display.value.length === 9) return;
-    if(display.value === '0') display.value = btn.value;
+    if(btn.value === '.') display.value += btn.value;
+    else if(display.value === '0') display.value = btn.value;
     else display.value += btn.value;
 }
 
@@ -13,14 +26,18 @@ var operador = ''
 var valor1 = 0
 function atualizarOperacao(btn){
     const display = document.getElementById('display');
-    operador = btn.value;
-    valor1 = parseInt(display.value);
-    display.value = '0';
+    operador = btn.getAttribute('jsValue');
+    valor1 = parseFloat(display.value);
+    if (operador === '**0.5') {
+        display.value = 0;
+        calcularOperacao();
+    } else display.value = '0';
+
 }
 
 function calcularOperacao(){
     const display = document.getElementById('display');
-    const valor2 = parseInt(display.value);
+    const valor2 = parseFloat(display.value);
     valor1 = eval(valor1+operador+valor2);
     display.value = valor1;
     operador = '';
