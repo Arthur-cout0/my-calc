1b187130970a1f92badf8f5378831250f190c60b
Arthur Couto <acm2@aluno.ifal.edu.br>
2022-05-10 14:30:53 -0300

    a

--- public/js/calc_functions.js
+++ public/js/calc_functions.js
@@ -1,46 +1,46 @@
 function brktSwitch(btn){
     if (btn.getAttribute('direction') === 'left'){
         atualizarDisplay(btn);
-        btn.setAttrbute('direction', 'right');
-        btn.setAttrbute('jsValue', ')');
+        btn.setAttribute('direction', 'right');
+        btn.value = ')';
     } else if (btn.getAttribute('direction') === 'right'){
         atualizarDisplay(btn);
-        btn.setAttrbute('direction', 'left');
-        btn.setAttrbute('jsValue', '(');
+        btn.setAttribute('direction', 'left');
+        btn.value = '(';
     }
+}
+
+function aumentarTamanhoDisplay(tamanho){
+    const display = document.getElementById('display');
+    display.style.fontSize = 'calc(10vmin /' + ((tamanho / 10) + 1) + ')';
 }
 
 function atualizarDisplay(btn){
     const display = document.getElementById('display');
-    if(display.value.length === 9) return;
+    console.log(btn.getAttribute('jsValue'));
+    if(display.value.length >= 9) aumentarTamanhoDisplay(display.value.length);
     if(btn.value === '.') display.value += btn.value;
     else if(display.value === '0') display.value = btn.value;
+    else if(btn.value === '√' || btn.value === '^') display.value += btn.getAttribute('jsValue');
     else display.value += btn.value;
 }
 
 function limparDisplay(){
     document.getElementById('display').value = '0';
+    display.style.fontSize = '10vmin';
 }
 
-var operador = ''
-var valor1 = 0
+var operacao = '';
 function atualizarOperacao(btn){
     const display = document.getElementById('display');
-    operador = btn.getAttribute('jsValue');
-    valor1 = parseFloat(display.value);
-    if (operador === '**0.5') {
-        display.value = 0;
-        calcularOperacao();
-    } else display.value = '0';
-
+    atualizarDisplay(btn);
 }
 
 function calcularOperacao(){
     const display = document.getElementById('display');
-    const valor2 = parseFloat(display.value);
-    valor1 = eval(valor1+operador+valor2);
+    operacao = display.value;
+    valor1 = eval(operacao);
     display.value = valor1;
-    operador = '';
 }       
 
 function manipularTeclado(){
